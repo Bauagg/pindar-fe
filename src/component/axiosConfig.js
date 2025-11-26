9a4b10aa5cc8e449c861c3de4a18426c15230058
dianpayno <dianpayno2023@gmail.com>
2025-11-26 22:25:00 +0700

    update axios

--- src/component/axiosConfig.js
+++ src/component/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: 'https://be.pindar.id/api', // Mengambil base URL dari .env
-  baseURL:"http://localhost:4000/api",
+  baseURL: 'https://be.pindar.id/api', // Mengambil base URL dari .env
+  // baseURL:"http://localhost:4000/api",
   headers: {
     "Content-Type": "application/json",
   },
